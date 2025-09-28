5f0f1b7520a6791908b0e8e855d79a255043a05b
Zirp302 <vkkakantakt@gmail.com>
2025-09-28 21:00:41 +0500

    Update Main.py
    Добавил название точек

--- Main.py
+++ Main.py
@@ -4,79 +4,80 @@ from pyglet import shapes as sh
 
 
 wind = pyglet.window.Window(720, 600)
-aa = sh.Circle(0, 0, 5)
-ab = sh.Circle(0, 0, 5)
+A = sh.Circle(0, 0, 5)
+B = sh.Circle(0, 0, 5)
 XY1 = [0, 0]
 XY2 = [0, 0]
-lab = pyglet.graphics.Batch() 
+pak = pyglet.graphics.Batch() 
 
 @wind.event
 def on_mouse_press(x, y, button, modifiers):
     # Первая точка
-    global line
     if button == mouse.LEFT:
-        global aa
-        aa = sh.Circle(x, y, 5, color=(255, 255, 255))
+        global A
+        global point_A
+        A = sh.Circle(x, y, 5, color=(255, 255, 255), batch=pak)
         XY1[0] = x
         XY1[1] = y
-
-        Y = XY1[0] - XY2[0]
-        X = XY1[1] - XY2[1]
-        line = sh.Line(XY1[0], XY1[1], XY2[0], XY2[1], 
-                        color=(255, 255, 255), thickness=10, batch=lab)
-
+        point_A = pyglet.text.Label(
+            'A',
+            x=XY1[0] - 5, y=XY1[1] + 15,
+            font_size=14, font_name='Arial',
+            color=(255, 255, 255), batch=pak)
     # Вторая точка
     elif button == mouse.RIGHT:
-        global ab
-        ab = sh.Circle(x, y, 5, color=(255, 255, 255))
+        global B
+        global point_B
+        B = sh.Circle(x, y, 5, color=(255, 255, 255), batch=pak)
         XY2[0] = x
         XY2[1] = y
+        point_B = pyglet.text.Label(
+            'B',
+            x=XY2[0] - 5, y=XY2[1] + 15,
+            font_size=14, font_name='Arial',
+            color=(255, 255, 255), batch=pak)
 
-        Y = XY1[0] - XY2[0]
-        X = XY1[1] - XY2[1]
-        line = sh.Line(XY1[0], XY1[1], XY2[0], XY2[1],
-                            color=(255, 255, 255), thickness=10, batch=lab)
-
-
-    otv = (X ** 2 + Y ** 2) ** 0.5
+    Y = XY1[0] - XY2[0]
+    X = XY1[1] - XY2[1]
+    str_A = f'A [x:{XY1[0]}  y:{XY1[1]}]'
+    str_B = f'B [x:{XY2[0]}  y:{XY2[1]}]'
+    otv = str((X ** 2 + Y ** 2) ** 0.5)
     print(otv, XY1, XY2)
-    
+    # Отрезок соеденяющий А и В
+    global line
+    line = sh.Line(
+        XY1[0], XY1[1], XY2[0], XY2[1],
+        color=(255, 255, 255), thickness=10,
+        batch=pak)
 
-    global label
-    global label_aa
-    global label_ab
-    str_aa = f'x:{XY1[0]}  y:{XY1[1]}'
-    str_ab = f'x:{XY2[0]}  y:{XY2[1]}'
-
-    label = pyglet.text.Label(
-        str(otv),
+            # Перенеси ЭТО в for
+    # Растоянние между А и В
+    global text_line
+    text_line = pyglet.text.Label(
+        otv,
         x=25, y=570,
-        font_size=16,
-        font_name='Arial',
-        color=(255, 255, 255), batch=lab)
-    
-    # Показывает координаты точки аа
-    label_aa = pyglet.text.Label(
-        str_aa,
-        x=25, y=550,
-        font_size=16,
-        font_name='Arial',
-        color=(255, 255, 255), batch=lab)
-    
-    # Показывает координаты точки аb
-    label_ab = pyglet.text.Label(
-        str_ab,
-        x=25, y=530,
-        font_size=16,
-        font_name='Arial',
-        color=(255, 255, 255), batch=lab)
+        font_size=16, font_name='Arial',
+        color=(255, 255, 255), batch=pak)
 
+    # Координаты точки A
+    global text_A
+    text_A = pyglet.text.Label(
+        str_A,
+        x=25, y=550,
+        font_size=16, font_name='Arial',
+        color=(255, 255, 255), batch=pak)
+
+    # Координаты точки В
+    global text_B
+    text_B = pyglet.text.Label(
+        str_B,
+        x=24, y=530,                    # х неодинковое так как В на 1px больше А при размере 16
+        font_size=16, font_name='Arial',
+        color=(255, 255, 255), batch=pak)
 
 @wind.event
 def on_draw():
     wind.clear()
-    aa.draw()
-    ab.draw()
-    lab.draw()
-    
+    pak.draw()
+
 pyglet.app.run() 
